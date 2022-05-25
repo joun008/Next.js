5e36dca1fff185dce3e95dec5db24d3965b7915e
joun008 <jouny007@gm.gist.ac.kr>
2022-05-26 04:46:48 +0900

    feat:prototype 1차

---
+++ next/components/header.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Link from "next/link";
+import Logo from "../components/svg/logo";
+import internal from "stream";
+export const result = new Map<number, number>();
+
+export default function Header() {
+  return (
+    <div className="flex max-w-md fixed top-0 w-full bg-white border-b-2">
+      <div className="pl-1 pr-1 p-2 flex flex-row w-full">
+        <Logo />
+        <div className="flex flex-1 items-center">
+          <div>채움이</div>
+        </div>
+        <div className="flex items-center border-2 bg-white border-gray-200 rounded-lg hover:bg-gray-200">
+          <Link href="/order">
+            <div>결제하기</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ next/pages/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Card from "../components/card";
+import Header from "../components/header";
+import Newcard from "../components/newcard";
+import { useProduct } from "../hooks/useProduct";
+import { typeCard } from "../type";
+export interface ICard {
+  typeCard;
+  key: number;
+}
+export default function Index() {
+  const [data, Setdata] = useState(0);
+  const { data: product, isLoading } = useProduct();
+  const onChange = (e: any) => {
+    console.log(e);
+    Setdata(e.target.value);
+  };
+  return (
+    <>
+      {!isLoading && product !== undefined ? (
+        <div className="flex flex-col justify-center items-center">
+          <Header />
+          <div className="flex flex-col justify-center items-center max-w mt-16">
+            {product.map(function (item: typeCard, index: number) {
+              return <Newcard key={index} props={item} />;
+            })}
+          </div>
+          <div className="flex flex-col justify-center items-center max-w space-y-5 pl-1 pr-1 mt-16">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+}
